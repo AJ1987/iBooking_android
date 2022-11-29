f4a8db623a1902e26ad92dd545acb9085c6829c4
AJ1987 <6680718@qq.com>
2022-11-30 01:40:33 +0800

    Add Login Interface Model

--- app/src/main/java/com/aj/ibooking/MainActivity.java
+++ app/src/main/java/com/aj/ibooking/MainActivity.java
@@ -1,14 +1,32 @@
 package com.aj.ibooking;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.aj.ibooking.activity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister = findViewById(R.id.btn_register);
     }
 }
